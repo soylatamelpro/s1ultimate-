2f0cb08782c8b76b65cc58b1b1862c304dd95e22
NotLatamel <itsswarmyhere@gmail.com>
2026-07-18 19:47:25 -0400

    Update Sonic ReactToItem.asm

--- _incObj/Sonic ReactToItem.asm
+++ _incObj/Sonic ReactToItem.asm
@@ -249,6 +249,8 @@ React_Monitor:
 React_Enemy:
 		tst.b	(v_invinc).w				; is Sonic invincible?
 		bne.s	.checkBossHit				; if yes, branch
+		cmpi.b	#id_SpinDash,obAnim(a0)			; is Sonic Spin Dashing? 
+		beq.s	.donthurtsonic				; if yes, branch
 		cmpi.b	#id_Roll,obAnim(a0)			; is Sonic rolling/jumping?
 		bne.w	React_ChkHurt				; if not, damage Sonic
 
